ee6150708592270af33a7e6e57376f8e473319b7
shouyamamoto <yamasyo0017@gmail.com>
2021-05-03 17:23:08 +0900

    [feat] checkColumn, changeColumnName

--- work20/userTable.js
+++ work20/userTable.js
@@ -1,6 +1,7 @@
-const fetchUsersURL = "https://jsondata.okiba.me/v1/json/XWziO210503032750"
+const fetchUsersURL = "https://jsondata.okiba.me/v1/json/kscum210503062703"
 
 const userTable = document.getElementById('userTable')
+const requiredColumns = ["id", "name", "sex", "age"] // 表示したいカラム
 
 const myFetch = async (fetchUsersURL) => {
   try {
@@ -18,56 +19,76 @@ const fetchUserData = () => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(myFetch(fetchUsersURL))
-    }, 3000)
+    }, 100)
   })
 }
 
 const createUserTable = async () => {
   const userData = await fetchUserData()
-  const { users, columns } = userData
-  createColumn(columns)
+  const users = await userData.users
+  createColumn(users)
   createUsers(users)
 }
 createUserTable()
 
 // カラムを生成する処理
-function createColumn(columns) {
+function createColumn(users) {
   const columnFragment = document.createDocumentFragment()
   const columnTr = document.createElement('tr')
+  let columns = []
+  columns = checkColumn(users, columns)
   columnTr.classList.add('t-head')
-
   columns.forEach(column => {
     const columnTh = document.createElement('th')
-    columnTh.textContent = column
+    columnTh.textContent = changeColumnName(column)
     columnFragment.appendChild(columnTh)
   })
-
   columnTr.appendChild(columnFragment)
   userTable.appendChild(columnTr)
+}
+
+// 表示したいカラムと、userが持っているkeysが同じかをチェックする
+function checkColumn(users, columns) {
+  users.forEach(user => {
+    const userKeys = Object.keys(user)
+    if (userKeys.toString === requiredColumns.toString) { // 表示したいカラムと、userが持っているkeysが同じであればそれをcolumnへ
+      columns = userKeys
+    }
+  })
+  return columns
+}
+
+// ユーザが持っているkeyを表示したいカラム名に変換して返す
+function changeColumnName(column) {
+  if (column === 'id') {
+    return 'ID'
+  } else if (column === 'name') {
+    return '名前'
+  } else if (column === 'age') {
+    return '年齢'
+  } else if (column === 'sex') {
+    return '性別'
+  }
 }
 
 // ユーザを生成する処理
 function createUsers(users) {
   const userDataFragment = document.createDocumentFragment()
-
   users.forEach(user => {
     const tr = document.createElement('tr')
     const tdId = document.createElement('td')
     const tdName = document.createElement('td')
     const tdSex = document.createElement('td')
     const tdAge = document.createElement('td')
-
     const { id, name, sex, age } = user
     tdId.textContent = id
     tdName.textContent = name
     tdSex.textContent = sex
     tdAge.textContent = age
-
     tr.appendChild(tdId)
     tr.appendChild(tdName)
     tr.appendChild(tdSex)
     tr.appendChild(tdAge)
-
     userDataFragment.appendChild(tr)
   })
   userTable.appendChild(userDataFragment)
