47023f6bd2973dc0678237e0b661fdff7b336790
shouyamamoto <yamasyo0017@gmail.com>
2021-05-08 19:53:36 +0900

    [feat] register

---
+++ work24/register.js
@@ -0,0 +1,51 @@
+const modal = document.getElementById('js-modal')
+const mask = document.getElementById('js-mask')
+const checkbox = document.getElementById('js-checkbox')
+const checkText = document.getElementById('js-checkText')
+const closeIcon = document.getElementById('js-closeIcon')
+const closeBtn = document.getElementById('js-closeBtn')
+const submitBtn = document.getElementById('js-submitBtn')
+
+checkText.addEventListener('click', () => {
+  modalOpen()
+})
+
+closeIcon.addEventListener('click', () => {
+  modalClose()
+})
+
+closeBtn.addEventListener('click', () => {
+  modalClose()
+})
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  if (checkbox.checked) {
+    location.href = './register-done.html'
+  } else {
+    alert('利用規約を最後まで読んでから送信ボタンを押してください')
+  }
+})
+
+modal.onscroll = function () {
+  const modalHeight = modal.clientHeight
+  const modalScrollHeight = modal.scrollHeight
+  const modalScrollTop = modal.scrollTop
+
+  if (modalScrollHeight - (modalHeight + modalScrollTop) === 0) {
+    checkbox.disabled = false
+    checkbox.checked = true
+  }
+}
+
+function modalClose() {
+  modal.classList.remove('active')
+  mask.classList.remove('active')
+  closeIcon.classList.remove('active')
+}
+
+function modalOpen() {
+  modal.classList.add('active')
+  mask.classList.add('active')
+  closeIcon.classList.add('active')
+}
